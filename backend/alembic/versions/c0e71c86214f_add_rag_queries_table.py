35f554e6bbb69936393a3d7161fef7514db6e0b6
Bhumi <bhumishirke77@gmail.com>
2026-05-28 20:22:41 +0530

    fix: remove duplicate alembic.ini keys and chain migration to head

--- backend/alembic/versions/c0e71c86214f_add_rag_queries_table.py
+++ backend/alembic/versions/c0e71c86214f_add_rag_queries_table.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'c0e71c86214f'
-down_revision: Union[str, Sequence[str], None] = None
+down_revision: Union[str, Sequence[str], None] = 'eb8060353ac6'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
