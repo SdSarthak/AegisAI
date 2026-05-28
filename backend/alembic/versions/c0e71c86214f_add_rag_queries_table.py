ea5f4c3bc03a1aec9c8802c00b4da557e8ecf0fd
Bhumi <bhumishirke77@gmail.com>
2026-05-28 20:29:37 +0530

    fix: update migration chain to point to correct head revision

--- backend/alembic/versions/c0e71c86214f_add_rag_queries_table.py
+++ backend/alembic/versions/c0e71c86214f_add_rag_queries_table.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'c0e71c86214f'
-down_revision: Union[str, Sequence[str], None] = 'eb8060353ac6'
+down_revision: Union[str, Sequence[str], None] = 'c3d9f1b2a4e6'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
